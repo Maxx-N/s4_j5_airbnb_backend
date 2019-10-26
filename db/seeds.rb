988e33b285be33136ae50c6ac765075980bfcbdb
Maxx-N <maximenorval@gmail.com>
2019-10-26 09:19:41 +0200

    affichage de toutes les propriétés d'un utilisateur

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,9 @@
     u.save
   end
 
-# Afficher les utilisateurs créés
+# Afficher les utilisateurs(trices) créé(e)s
+  puts "\n"
+  puts "Voici la liste des #{User.all.length} utilisateurs :"
   puts "\n"
   tp User.all, :id, :email, :phone_number
 
@@ -30,10 +32,12 @@
 
 # Afficher les villes créées
   puts "\n"
+  puts "Voici la liste des #{City.all.length} villes disponibles :"
+  puts "\n"
   tp City.all, :id, :name, :zip_code
 
-# Créer 10 logements
-  10.times do |index|
+# Créer 100 logements
+  100.times do |index|
     a = Accomodation.new
     a.id = index + 1
     a.available_beds = rand(1..8)
@@ -41,21 +45,30 @@
     a.description = Faker::Lorem.paragraph_by_chars(number: 145)
     a.has_wifi = [true, false].sample
     a.welcome_message = "Bonjour! Passez un bon séjour dans notre magnifique logement !"
-    a.city = City.all.find(rand(1..City.all.length))
-    a.administrator = User.all.find(rand(1..User.all.length))
+    a.city = City.all[rand(0..(City.all.length - 1))]
+    a.administrator = User.all[rand(0..(User.all.length - 1))]
     a.save
   end
 
-# Afficher les logements créés
+# Afficher les logements créés :
+    puts "\n"
+    puts "Voici la liste des #{Accomodation.all.length} logements disponibles :"
     puts "\n"
     tp Accomodation.all, :id, :available_beds, :price, :has_wifi, :administrator_id, :city_id
 
-# Prend un logement au hasard et affiche son nombre de places, sont prix, sa ville et l'email de son propriétaire
-  a = Accomodation.all.sample
-  puts "\n"
-  puts "L'utilisateur ayant l'email \"#{a.administrator.email}\" propose #{a.available_beds} couchages à #{a.price} € / nuit dans la ville de #{a.city.name}"
 
-    
+# Prendre un utilisateur au hasard, affiche son mail, ses logements et son numéro de téléphone :
+  puts "\n"
+  my_user = User.all.sample
+  puts "Voici la liste des logements proposés par \"#{my_user.email}\" :"
+  puts "\n"
+  my_user_properties = my_user.owned_accomodations
+  my_user_properties.each do |a|
+    puts "     - #{a.available_beds} couchages à #{a.city.name} (#{a.city.zip_code}), pour #{a.price}€/nuit"
+  end
+  puts "\n"
+  puts "Vous pouvez le(la) contacter au : #{my_user.phone_number}"
+
     
 
   
